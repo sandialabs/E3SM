1ff5fa25f252930287ecd078e859cec3b72423d4
Joe Hollowed <jphollowed@gmail.com>
2023-06-27 14:09:45 -0600

    cleaning comment

--- components/eam/src/physics/cam/cldera_dynamic_tracers.F90
+++ components/eam/src/physics/cam/cldera_dynamic_tracers.F90
@@ -28,7 +28,7 @@ module cldera_dynamic_tracers
   public :: cldera_dynamic_tracers_implements_cnst ! true if constituent is implemented by this package
   public :: cldera_dynamic_tracers_is_pt
   public :: cldera_dynamic_tracers_is_pv
-  public :: cldera_dynamic_tracers_is_enabled ! true if constituent is implemented by this package
+  public :: cldera_dynamic_tracers_is_enabled
   public :: cldera_dynamic_tracers_init            ! initialize history fields, datasets
   public :: cldera_dynamic_tracers_init_cnst       ! initialize constituent field
   public :: cldera_dynamic_tracers_timestep_tend
